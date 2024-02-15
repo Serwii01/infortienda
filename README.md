bb10f22b40ac52b0f822485e1263deab524e107a
Serwii01 <133500002+Serwii01@users.noreply.github.com>
2024-02-15 08:45:13 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+Pagina de la infortienda.
+
+-HTML5, CSS3 y JAVASCRIPT.
+
+-Responsividad mediante media querys.
+
+-Formulario con validación js.
+
+-Archivos guardados en caché con la página.
+
+-Scroll reveal.
---
+++ scroll.js
@@ -0,0 +1,99 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Initialize ScrollReveal
+    ScrollReveal().reveal('.header', {
+        duration: 1000,
+        delay: 200,
+        distance: '50px',
+        origin: 'bottom',
+        reset: false
+    });
+    
+    ScrollReveal().reveal('.slider-container', {
+        duration: 1000,
+        delay: 200,
+        distance: '50px',
+        origin: 'bottom',
+        reset: false
+    });
+
+    ScrollReveal().reveal('.botones', {
+        duration: 1000,
+        delay: 200,
+        distance: '50px',
+        origin: 'bottom',
+        reset: false
+    });
+
+    ScrollReveal().reveal('.buscador', {
+        duration: 1000,
+        delay: 200,
+        distance: '50px',
+        origin: 'bottom',
+        reset: false
+    });
+
+    ScrollReveal().reveal('.seccion', {
+        duration: 1000,
+        delay: 200,
+        distance: '50px',
+        origin: 'bottom',
+        reset: false
+    });
+
+    ScrollReveal().reveal('.articulo', {
+        duration: 1000,
+        delay: 200,
+        distance: '50px',
+        origin: 'bottom',
+        reset: false
+    });
+
+    ScrollReveal().reveal('.articulo', {
+        duration: 1000,
+        delay: 200,
+        distance: '50px',
+        origin: 'bottom',
+        reset: false
+    });
+
+    ScrollReveal().reveal('.newsletter', {
+        duration: 1000,
+        delay: 200,
+        distance: '50px',
+        origin: 'bottom',
+        reset: false
+    });
+
+    ScrollReveal().reveal('.imgs', {
+        duration: 1000,
+        delay: 200,
+        distance: '50px',
+        origin: 'bottom',
+        reset: false
+    });
+
+    ScrollReveal().reveal('.faq', {
+        duration: 1000,
+        delay: 200,
+        distance: '50px',
+        origin: 'bottom',
+        reset: false
+    });
+
+    ScrollReveal().reveal('.question', {
+        duration: 1000,
+        delay: 200,
+        distance: '50px',
+        origin: 'bottom',
+        reset: false
+    });
+
+    ScrollReveal().reveal('.contacto', {
+        duration: 1000,
+        delay: 200,
+        distance: '50px',
+        origin: 'bottom',
+        reset: false
+    });
+
+});
